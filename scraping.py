7c1240433cf66b95be36bfedd6822d6abea5b331
Gourav-Bangad <100506129+Gourav-Bangad@users.noreply.github.com>
2023-02-02 17:19:25 +0530

    Scrapping of Specifications from every mobile page

--- scraping.py
+++ scraping.py
@@ -5,32 +5,45 @@ from bs4 import BeautifulSoup
 
 Product_name = []
 Price = []
-Description = []
+Specifications = []
+Product_link=[]
 
-for i in range(2,248):
+
+for i in range(1,245):
+              
               url ="https://www.flipkart.com/search?q=mobile&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off&page="+str(i)
 
               r = requests.get(url)
 
               soup = BeautifulSoup(r.text,"lxml")
 
-              names = soup.find_all("div",class_="_4rR01T")
+              data = soup.find_all("div",class_="_2kHMtA")
 
-              prices = soup.find_all("div",class_="_30jeq3 _1_WHN1")
-
-              desc = soup.find_all("ul",class_="_1xgFaf")    
-
-              for i in names:
-                    temp = i.text
-                    Product_name.append(temp)   
-              for i in prices:
-                    temp = i.text
-                    Price.append(temp)                
-              for i in desc:
-                    temp = i.text
-                    Description.append(temp)
+              for items in data:
+                    link = items.find("a", href=True)["href"]
+                    Product_link.append("https://www.flipkart.com"+link)
 
 
-df = pd.DataFrame({"Product Name":Product_name,"Prices":Price,"Description":Description})
 
-df.to_csv("C:/Users/MI/Desktop/Dsci/mobile_data1.csv")
+for p_url in Product_link:
+            r = requests.get(p_url)
+            soup = BeautifulSoup(r.text,"lxml")
+            # box = soup.find_all("div",class_="_1YokD2 _3Mn1Gg col-8-12")
+            name = soup.find("span",class_="B_NuCI")
+            price = soup.find("div",class_="_30jeq3 _16Jk6d")
+            specs =soup.find("div",class_="_1UhVsV")
+            try:
+                  Product_name.append(name.text)
+            except AttributeError:
+                  Product_name.append("")
+            try:
+                  Price.append(price.text)
+            except AttributeError:
+                  Price.append("")
+            try:
+                  Specifications.append(specs.text)
+            except AttributeError:
+                  Specifications.append("")
+
+df = pd.DataFrame({"Product Name":Product_name,"Price":Price,"Specifications":Specifications})
+df.to_csv("C:/Users/MI/Desktop/Dsci/New_Scraping.csv")
